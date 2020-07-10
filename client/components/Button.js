f8a610bbe35414fb74bc51ba6cd0e843db58bec2
resjudicoda <adam.j.reis@gmail.com>
2020-07-10 18:19:26 -0500

    playing with css to remove tooltip

--- client/components/Button.js
+++ client/components/Button.js
@@ -4,7 +4,14 @@ const Button = props => (
   <div className="buttons fadein">
     <div className="button">
       <label htmlFor="single" />
-      <input type="file" id="single" onChange={props.onChange} />
+      <input
+        type="file"
+        id="single"
+        onChange={props.onChange}
+        accept="image/*"
+        title=" "
+        value=""
+      />
     </div>
   </div>
 )
